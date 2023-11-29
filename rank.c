42a47ad2c305a21e9251f12b36b5e7aed29fef00
gongweijing <876887913@qq.com>
2023-11-29 09:30:52 +0800

    add

--- rank.c
+++ rank.c
@@ -28,6 +28,7 @@ void assign_rank_and_crowding_distance (NSGA2Type *nsga2Params, population *new_
     cur->parent = NULL;
     cur->child = NULL;
     temp1 = orig;
+    // 初始化orig种群集的双向链表
     for (i=0; i<nsga2Params->popsize; i++)
     {
         insert (temp1,i);
@@ -35,27 +36,32 @@ void assign_rank_and_crowding_distance (NSGA2Type *nsga2Params, population *new_
     }
     do
     {
+        // 如果到达了orig最后一个元素,那么将对应元素的crowd_distance赋值为INF
         if (orig->child->child == NULL)
         {
             new_pop->ind[orig->child->index].rank = rank;
             new_pop->ind[orig->child->index].crowd_dist = INF;
             break;
         }
+        // temp1为origin原本的第一个种群，将其插入cur中，并删除origin原本的第一个种群
         temp1 = orig->child;
         insert (cur, temp1->index);
         front_size = 1;
         temp2 = cur->child;
         temp1 = del (temp1);
         temp1 = temp1->child;
+        // 第一层循环：origin不断遍历到child，直到到达空集结束；
         do
         {
             temp2 = cur->child;
+            // 第二层循环：1)cur不断遍历到child，直到到达空集结束；2)origin对应节点被cur对应节点支配结束；
             do
             {
                 end = 0;
                 flag = check_dominance (nsga2Params, &(new_pop->ind[temp1->index]), &(new_pop->ind[temp2->index]));
                 if (flag == 1)
                 {
+                    // 将cur内的节点放入origin，并从cur中将其删除，前沿数-1.
                     insert (orig, temp2->index);
                     temp2 = del (temp2);
                     front_size--;
@@ -73,6 +79,7 @@ void assign_rank_and_crowding_distance (NSGA2Type *nsga2Params, population *new_
             while (end!=1 && temp2!=NULL);
             if (flag == 0 || flag == 1)
             {
+                // 将origin中不被cur上个结点支配的解放入cur中,作为候选前沿
                 insert (cur, temp1->index);
                 front_size++;
                 temp1 = del (temp1);
