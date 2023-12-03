d03e22c4cb96376b2f17447f7bf623ab28d73af3
gongweijing <876887913@qq.com>
2023-12-03 13:30:27 +0800

    add config proxy

--- proxy/get_host_ip.sh
+++ proxy/get_host_ip.sh
@@ -8,3 +8,11 @@ export https_proxy=http://${hostip}:${port}
 export http_proxy=http://${hostip}:${port} 
 export all_proxy=socks5://${hostip}:${port}
 echo "all proxy is config successfully!"
+echo "initial git proxy as follows:"
+echo "git_porxy_http is:"$(git config --global --get http.proxy)
+echo "git_porxy_https is:"$(git config --global --get https.proxy)
+git config --global http.proxy "http://${hostip}:${port}"
+git config --global https.proxy "http://${hostip}:${port}"
+echo "new git proxy as follows:"
+echo "git_porxy_http is:"$(git config --global --get http.proxy)
+echo "git_porxy_https is:"$(git config --global --get https.proxy)
