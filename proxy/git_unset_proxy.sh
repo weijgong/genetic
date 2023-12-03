ba5dabd8966b05fb46d6e8ec33c214c429818ff0
gongweijing <876887913@qq.com>
2023-12-03 13:36:57 +0800

    set the proxy of the windows

--- proxy/git_unset_proxy.sh
+++ proxy/git_unset_proxy.sh
@@ -1,2 +1,5 @@
 git config --global --unset http.proxy
 git config --global --unset https.proxy
+echo "reseted git proxy as follows:"
+echo "git_porxy_http is:"$(git config --global --get http.proxy)
+echo "git_porxy_https is:"$(git config --global --get https.proxy)
