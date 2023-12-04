314daf88173e9fe6b7421feadf4dace838b8c6c1
gongweijing <876887913@qq.com>
2023-12-04 08:03:24 +0800

    测试距离函数

--- sat_algorithm/read_position.c
+++ sat_algorithm/read_position.c
@@ -2,7 +2,7 @@
  * @Author: gongweijing 876887913@qq.com
  * @Date: 2023-12-03 00:17:15
  * @LastEditors: gongweijing 876887913@qq.com
- * @LastEditTime: 2023-12-03 20:42:20
+ * @LastEditTime: 2023-12-03 23:02:04
  * @FilePath: /gongweijing/nsga2/sat_algorithm/read_position.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,8 +12,8 @@
 void init_position(){
     FILE *file;
     // 文件路径
-    // const char *file_path = "/root/genetic/sat_data/Target1_LLA_Position.csv";
-     const char *file_path = "/home/gongweijing/nsga2/sat_data/Target1_LLA_Position.csv";
+    const char *file_path = "/root/genetic/sat_data/Target1_LLA_Position.csv";
+    //  const char *file_path = "/home/gongweijing/nsga2/sat_data/Target1_LLA_Position.csv";
     // 打开文件以供读取
     file = fopen(file_path, "r");
     // 检查文件是否成功打开
@@ -33,5 +33,7 @@ void init_position(){
 }
 int main() {
     init_position();
+    printf("%f\n",compute_curve_distance(pos_list[0],pos_list[1]));
+    printf("%f\n",compute_haversine_distance(pos_list[0],pos_list[1]));
     return 0;  // 返回零表示程序执行成功
 }
